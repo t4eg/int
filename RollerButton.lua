f53530b40318b161ea143ca60d3dd87d1be38259
t4eg <adam_mazur@interia.eu>
2016-12-02 19:41:19 +0100

    rollerbutton

---
+++ RollerButton.lua
@@ -0,0 +1,27 @@
+local RollerButton = Classes.class()
+
+function RollerButton:init(buttonUp, buttonDown)
+    self.buttonUp = buttonUp
+    self.buttonDown = buttonDown
+    self.buttonUp:onXXXXXXXXXX(function() self:onClick("up") end)
+    self.buttonDown:onXXXXXXXXXX(function() self:onClick("down") end)
+end
+
+function RollerButton:setRoller(...)
+    self.rollers = arg
+end
+
+function RollerButton:onClick(direction)
+    if self.rollers == nil then
+        return
+    end
+    for i, roller in ipairs(self.rollers) do
+        if direction == "up" then
+            roller:open()
+        elseif direction == "down" then
+            roller:close()
+        end
+    end
+end
+
+return RollerButton
